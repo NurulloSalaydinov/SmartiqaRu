3abb5939d18b04481f9b6730daebcab7fad02f21
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2021-11-13 10:16:26 +0300

    SmartiqaRu's first page finished

--- js/app.js
+++ js/app.js
@@ -19,4 +19,27 @@ const App = {
         },
     }
 }
-const app = Vue.createApp(App).mount('#app')
+const app = Vue.createApp(App).mount('#app');
+
+const App1 = {
+	data(){
+		return {
+			comment:true,
+			times:false,
+			icons:false
+		}
+	},
+	methods: {
+		show: function () {
+			this.comment = false;
+			this.times = true;
+			this.icons = true;
+		},
+		hide: function() {
+			this.comment = true;
+			this.times = false;
+			this.icons = false;
+		}
+	}
+}
+const app1 = Vue.createApp(App1).mount("#app1");
